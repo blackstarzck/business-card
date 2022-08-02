ed1970069909e4308460489daa5fbd49d86d8b29
chanki <bucheongosok@gmail.com>
2022-08-02 16:56:53 +0900

    로그인화면 완성

--- src/components/login/login.jsx
+++ src/components/login/login.jsx
@@ -1,29 +1,35 @@
-import React from 'react'
+import React from 'react';
 import Footer from '../footer/footer';
 import Header from '../header/header';
+import styles from './login.module.css';
 
 const Login = ({ authService }) => {
-    const onLogin = (event) => {
-        authService //
-        .login(event.currentTarget.textContent)
-        .then(
-            console.log()
-        );
-    }
+  const onLogin = event => {
+    authService //
+      .login(event.currentTarget.textContent)
+      .then(console.log);
+  };
+  return (
+    <section className={styles.login}>
+      <Header />
+      <section>
+        <h1>Login</h1>
+        <ul className={styles.list}>
+          <li className={styles.item}>
+            <button className={styles.button} onClick={onLogin}>
+              Google
+            </button>
+          </li>
+          <li className={styles.item}>
+            <button className={styles.button} onClick={onLogin}>
+              Github
+            </button>
+          </li>
+        </ul>
+      </section>
+      <Footer />
+    </section>
+  );
+};
 
-    return (
-        <section>
-            <Header/>
-            <section>
-                <h1>Login</h1>
-                <ul>
-                    <li><button onClick={onLogin}>Google</button></li>
-                    <li><button onClick={onLogin}>Github</button></li>
-                </ul>
-            </section>
-            <Footer/>
-        </section>
-    );
-}
-
-export default Login
+export default Login;
